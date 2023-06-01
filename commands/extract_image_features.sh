64c91b292384ac29725618ade2134269af75139e
Nguyễn Đặng Tuấn Kiệt <tuankietnk2001@gmail.com>
2023-06-01 18:58:10 +0700

    Delete unnecessary commands

--- commands/extract_image_features.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CHECKPOINT_DIR="models/model.ckpt-34865"
-
-python -B src/run_inference.py \
-  --checkpoint_path=${CHECKPOINT_DIR} \
-  --item_ids="data/label/test_no_dup.json" \
-  --image_dir="../../Back-end/public/images/items/" \
-  --feature_file="data/features/image_features.pkl" \
-  --rnn_type="lstm"
